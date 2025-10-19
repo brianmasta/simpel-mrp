dc9e2e8caf56dd824810349ce759c582407e7fc4
Brian Marandof <brianmasta23@gmail.com>
2025-10-19 16:30:39 +0900

    kembalikan auth kesemula

--- resources/views/components/layouts/auth.blade.php
+++ resources/views/components/layouts/auth.blade.php
@@ -2,25 +2,14 @@
 
 <html lang="en">
   <head>
-  <title>SIMPEL-MRP - Sistem Pelayanan Surat OAP Papua Tengah</title>
-  <meta name="description" content="Platform resmi MRP Papua Tengah untuk pengajuan dan verifikasi Surat Orang Asli Papua (OAP) secara online. Cepat, sah, dan terverifikasi.">
-  <meta name="keywords" content="SIMPEL MRP, Surat OAP, Pelayanan OAP, MRP Papua Tengah, Surat Orang Asli Papua Online">
-
-  <!-- OPEN GRAPH / SHARE PREVIEW -->
-  <meta property="og:title" content="SIMPEL-MRP - Layanan Surat OAP Papua Tengah">
-  <meta property="og:description" content="Pengajuan surat OAP online secara resmi dan terverifikasi oleh MRP Papua Tengah.">
-  <meta property="og:type" content="website">
-  <meta property="og:url" content="https://simpelmrp.com">
-  <meta property="og:site_name" content="MRP Papua Tengah">
-  <meta property="og:image" content="{{ asset('assets/img/logo.png') }}" />
-
-  <!-- TWITTER CARD -->
-  <meta name="twitter:card" content="summary_large_image">
-  <meta name="twitter:title" content="SIMPEL-MRP - Layanan Surat OAP Papua Tengah">
-  <meta name="twitter:description" content="Pengajuan surat OAP online secara resmi dan terverifikasi oleh MRP Papua Tengah.">
-  <meta name="twitter:image" content="{{ asset('assets/img/logo.png') }}">
-
-
+    <base href="./">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <meta name="description" content="CoreUI - Open Source Bootstrap Admin Template">
+    <meta name="author" content="Łukasz Holeczek">
+    <meta name="keyword" content="Bootstrap,Admin,Template,Open,Source,jQuery,CSS,HTML,RWD,Dashboard">
+    <title>MRP - Provinsi Papua Tengah</title>
     <link rel="apple-touch-icon" sizes="57x57" href="{{ asset('assets/img/logo.png') }}">
     <link rel="apple-touch-icon" sizes="60x60" href="{{ asset('assets/img/logo.png') }}">
     <link rel="apple-touch-icon" sizes="72x72" href="{{ asset('assets/img/logo.png') }}">
