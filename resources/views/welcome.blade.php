8c3ff4b7cc52d55c54a42d63909902226b9ed097
Brian Marandof <brianmasta23@gmail.com>
2026-02-04 15:34:10 +0900

    tambahan link video tutorial pada halaman welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -127,7 +127,7 @@
             <p data-aos="fade-up" data-aos-delay="100"><strong>Sistem Informasi Pelayanan Majelis Rakyat Papua Provinsi Papua Tengah</strong> Berkomitmen memberikan layanan digital yang cepat, transparan, dan akuntabel untuk mendukung pelestarian adat, budaya, serta perlindungan hak-hak masyarakat adat Papua dalam kerangka Otonomi Khusus.</p>
             <div class="d-flex flex-column flex-md-row" data-aos="fade-up" data-aos-delay="200">
               <a href="/register" class="btn-get-started">Buat Akun <i class="bi bi-arrow-right"></i></a>
-              <a href="#" class="glightbox btn-watch-video d-flex align-items-center justify-content-center ms-0 ms-md-4 mt-4 mt-md-0"><i class="bi bi-play-circle"></i><span>Watch Video</span></a>
+              <a href="https://youtu.be/3nHLR9tJIQo" class="glightbox btn-watch-video d-flex align-items-center justify-content-center ms-0 ms-md-4 mt-4 mt-md-0"><i class="bi bi-play-circle"></i><span>Watch Video</span></a>
             </div>
           </div>
           <div class="col-lg-6 order-1 order-lg-2 hero-img" data-aos="zoom-out">
